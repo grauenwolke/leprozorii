0a3fdda248080bfc8ea3195b4242400407501b28
Natalya Makarova <Grauen.Wolke@gmail.com>
2022-10-20 06:44:17 +0300

    fix(lw0): вторая попытка формата документа

--- lw0/funcSumTime.php
+++ lw0/funcSumTime.php
@@ -1,9 +1,9 @@
 <?php
-function sumTime(string $firstTime,string $secondTime): string
+function sumTime(string $firstTime, string $secondTime): string
 {
-    if (strtotime($firstTime) === false || strtotime($secondTime) === false){
+    if (strtotime($firstTime) === false || strtotime($secondTime) === false) {
         return 'incorrect input';
-	}
+    }
     return date('H:i:s', strtotime($firstTime) + strtotime($secondTime));
 }
-echo sumTime('10:30:30','11:12:13');
+echo sumTime('10:30:30', '11:12:13');
